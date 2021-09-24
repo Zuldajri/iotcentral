f48f19c872582dd8a4e11bd81c714871f233cdef
Zuldajri <w.matyas15@hotmail.fr>
2021-09-24 19:28:12 +0200

    Update aziotcia.sh

--- aziotcia.sh
+++ aziotcia.sh
@@ -61,7 +61,7 @@ echo ""
 echo "Running setup-docker"
 
 echo "Installing docker-compose"
-curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 echo "Installing Docker"
